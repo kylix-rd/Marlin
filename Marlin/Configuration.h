2006adef4ce3604312116b1f76d3ca8c34248e9d
Allen Bauer <kylix.rd@gmail.com>
2021-02-15 19:03:59 -0800

    BTT002 bugfix Orange Bear

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -998,7 +998,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 23, 5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 27, 5, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
